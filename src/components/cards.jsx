cf1800ff8846f44b88cbfaffa00f6e3df643db5d
David Bouhaben <davidbouhaben@yahoo.co.id>
2025-12-11 22:15:31 +0700

    add Font Awesome icons and update Cards component structure

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Cards() {
 const [todos, setTodos] = useState([
-  {
-    type: "grocery",
-    task: "sing",
-    completed: true, // Or false
-  },
-]);
+  { id: 1, list: "Buy groceries", time: "9:00 AM" },
+  { id: 2, list: "Finish homework", time: "11:00 AM" },
+  { id: 3, list: "Clean the house", time: "2:00 PM" },
+  { id: 4, list: "Pay bills", time: "4:30 PM" },
+  { id: 5, list: "Call a friend", time: "7:00 PM" },
+  ]);
+
   return (
     <div>
       
